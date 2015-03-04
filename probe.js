7c496c6b68fc217a1ae4c3647313edfc43c76d3b
Andrey Yatskov <andrey.yatskov@silvertreesystems.com>
2015-03-04 12:18:01 +0300

    each dtrace consumer starts into separate process

--- probe.js
+++ probe.js
@@ -1,79 +1,50 @@
 var http = require('http');
-var libdtrace = require('libdtrace');
 var express = require('express');
 var path = require('path');
-var exec = require('child_process').exec;
+var child_process = require('child_process');
+var exec = child_process.exec;
 
 /* create our express & http server and prepare to serve javascript files in ./public
-*/
+ */
 var app = express()
-  , http = require('http')
   , server = http.createServer(app)
   , WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({server: server});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* Before we go any further we must realize that each time a user connects we're going to want to
-   them send them dtrace aggregation every second. We can do so using 'setInterval', but we must
-   keep track of both the intervals we set and the dtrace consumers that are created as we'll need
-   them later when the client disconnects.
-*/
-var interval_id_by_session_id = {};
-var dtp_by_session_id = {};
-
-/* In order to effecienctly send packets we're going to use the Socket.IO library which seemlessly
-   integrates with express.
-*/
-
 /* Now that we have a web socket server, we need to create a handler for connection events. These
-   events represet a client connecting to our server */
-var clientId = 0;
-wss.on('connection', function (ws) {
-    var thisId = ++clientId;
+ events represet a client connecting to our server */
+wss.on('connection', function(ws) {
+    var consumer = child_process.fork('dtrace-consumer.js');
+
     /* Like the web server object, we must also define handlers for various socket events that
-       will happen during the lifetime of the connection. These will define how we interact with
-       the client. The first is a message event which occurs when the client sends something to
-       the server. */
-    ws.on('message', function (message) {
-        /* The only message the client ever sends will be sent right after connecting.
-           So it will happen only once during the lifetime of a socket. This message also
-           contains a d script which defines an agregation to walk.
-        */
-        var dtp = new libdtrace.Consumer();
+     will happen during the lifetime of the connection. These will define how we interact with
+     the client. The first is a message event which occurs when the client sends something to
+     the server. */
+    ws.on('message', function(message) {
 
-        dtp.strcompile(message);
-        dtp.go();
-        dtp_by_session_id[thisId] = dtp;
+        consumer.send({type: 'heatmapScript', message: message});
 
+        consumer.on('message', function (msg) {
+            try {
+                ws.send(msg);
+            }
+            catch (e) {
+                console.log('websocket error happens');
+            }
+        });
 
-         /* All that's left to do is send the aggration data from the dscript.  */
-         interval_id_by_session_id[thisId] = setInterval(function () {
-             var aggdata = {};
-             try {
-                 dtp.aggwalk(function (id, key, val) {
-                     for( index in val ) {
-                         aggdata[key] = val;
-                     }
-                 });
-                 ws.send(JSON.stringify(aggdata));
-             } catch( err ) {
-                 console.log(err);
-             }
-         }, 1101);
     });
-
-
     /* Not so fast. If a client disconnects we don't want their respective dtrace consumer to
-       keep collecting data any more. We also don't want to try to keep sending anything to them
-       period. So clean up. */
-    ws.on('close', function () {
-        clearInterval(clearInterval(interval_id_by_session_id[thisId]));
-        var dtp = dtp_by_session_id[thisId];
-        if (dtp && dtp.hasOwnProperty('stop')) {
-            dtp.stop();
-        }
-        delete dtp_by_session_id[thisId];
+     keep collecting data any more. We also don't want to try to keep sending anything to them
+     period. So clean up. */
+    ws.on('close', function() {
+        consumer.disconnect();
+        consumer.on('exit', function(code, signal) {
+            console.log('dtrace process exit with code %s and signal %s', code, signal);
+        });
+        consumer.kill();
         console.log('disconnected');
     });
 });
